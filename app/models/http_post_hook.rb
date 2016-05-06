eb89f1ee8fcddc35697c17614d11207bb936e4e3
Peter Graham <peterghm@gmail.com>
2016-05-06 01:13:30 -0400

    Add better URL validation

--- app/models/http_post_hook.rb
+++ app/models/http_post_hook.rb
@@ -1,8 +1,7 @@
 class HttpPostHook < Hook
   store_accessor :config, :post_urls
 
-  # TODO - ensure that it's an Array, and validate URLs by calling URI.parse
-  validates :post_urls, length: {minimum: 1}
+  validate :validate_post_urls
 
   def execute!(forwardable_params)
     post_urls.each do |url|
@@ -13,6 +12,26 @@ class HttpPostHook < Hook
         RestClient.post(url, query)
       rescue => e
         # TODO: don't fail silently
+      end
+    end
+  end
+
+private
+
+  def validate_post_urls
+    if !post_urls.is_a?(Array) || post_urls.empty?
+      errors.add(:post_urls, "must specify at least one URL")
+      return
+    end
+
+    post_urls.each do |url|
+      begin
+        uri = URI.parse(url)
+        if !%w[http https].include?(uri.scheme)
+          errors.add(:post_urls, "scheme must be http or https, but got: #{url}")
+        end
+      rescue
+        errors.add(:post_urls, "has invalid URL: #{url}")
       end
     end
   end
--- spec/models/http_post_hook_spec.rb
+++ spec/models/http_post_hook_spec.rb
@@ -1,9 +1,21 @@
 describe HttpPostHook do
   describe "validations" do
     context "without any post_urls specified" do
-      subject { FactoryGirl.build(:http_post_hook, post_urls: []) }
+      subject { FactoryGirl.build(:http_post_hook, post_urls: []).tap(&:valid?).errors.messages }
 
-      it { is_expected.not_to be_valid }
+      it { is_expected.to eq(post_urls: ["must specify at least one URL"]) }
+    end
+
+    context "with an invalid post_urls format specified" do
+      subject { FactoryGirl.build(:http_post_hook, post_urls: ["  "]).tap(&:valid?).errors.messages }
+
+      it { is_expected.to eq(post_urls: ["has invalid URL:   "]) }
+    end
+
+    context "with an invalid post_urls scheme specified" do
+      subject { FactoryGirl.build(:http_post_hook, post_urls: ["ftp://google.com", "git@github.com"]).tap(&:valid?).errors.messages }
+
+      it { is_expected.to eq(post_urls: ["scheme must be http or https, but got: ftp://google.com", "scheme must be http or https, but got: git@github.com"]) }
     end
   end
 
